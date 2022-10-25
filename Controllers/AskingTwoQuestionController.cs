83ca9d0d9d1e35b782370953efd5275981b46794
kent2pas <kentcejotupas@gmail.com>
2022-10-25 15:03:27 -0700

    AskingTwoQuestionAPI

---
+++ Controllers/AskingTwoQuestionController.cs
@@ -0,0 +1,24 @@
+//
+//
+//
+//Peer Review: Elizar Garcia, code works great. i really like the layout of how the questions are asked and followed up with. The code shows up on postman with no problem. Good job sir!
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace TupasKAskingTwoQuestionsAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AskingTwoQuestionController : ControllerBase
+{
+ [HttpGet]
+ [Route("Questions/{name}/{adj}/{number}")] 
+
+ public string Questions(string name, string adj, string number)
+ {
+    return $"Hey {name} \nHow are you feeling? \n{adj} \nSoooo what time did you wake up? \n{number} \nThats cool {name} waking up at {number}";
+ }
+}
